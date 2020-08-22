fb714b9ccee3f1f6fb389737421d0f6c7193f2a5
Uchechukwu Ugwuanyi <denisu29@gmail.com>
2020-08-22 14:59:42 +0000

    minor soemthing

--- lookingglass/urls.py
+++ lookingglass/urls.py
@@ -19,6 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('lg/', include('lg.urls')),
+    path('', include('lg.urls')),
 
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
