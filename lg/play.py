49bc8fc2cc47d35bd6c767d0361c848aaee22978
Ugwuanyi Uchechukwu <denisu29@gmail.com>
2021-04-13 19:07:18 +0100

    updated form to include new members and modified overall layout for eg changed size of side and main

--- lg/play.py
+++ lg/play.py
@@ -1,7 +1,14 @@
-with open('/home/uchechukwu/Desktop/lookingglass/lg/tt.txt') as result:
-
-    a = result.read()[1:]
-    print(a)
-   
+import re
+nl = '\n'
+def split_on_empty_lines(s):
     
-   
+    # greedily match 2 or more new-lines
+    blank_line_regex = r"(?:\r?\n){2,}"
+    return re.split(blank_line_regex, s.strip())
+
+
+with open('/home/uchechukwu/Documents/members_in_rc.txt') as result:
+    a = result.read()
+
+    for item in a.splitlines():
+        print(f'<option value="{item}">{item.upper()}</optiom>')
